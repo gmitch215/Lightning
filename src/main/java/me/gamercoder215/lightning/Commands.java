f3e312c00cfc8e5a14941dba2ff129fd96524834
GamerCoder215 <gamercoder215@gmail.com>
2022-04-14 22:39:14 -0500

    Update Commands.java

--- src/main/java/me/gamercoder215/lightning/Commands.java
+++ src/main/java/me/gamercoder215/lightning/Commands.java
@@ -5,117 +5,106 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 
-import revxrsal.commands.annotation.Command;
-import revxrsal.commands.annotation.Description;
-import revxrsal.commands.annotation.Range;
-import revxrsal.commands.annotation.Switch;
-import revxrsal.commands.annotation.Usage;
-import revxrsal.commands.bukkit.annotation.CommandPermission;
-
-public class Commands {
+public class Commands implements CommandExecutor {
 
     protected Lightning plugin;
 
     public Commands(Lightning plugin) {
         this.plugin = plugin;
+        plugin.getCommand("lserver").setExecutor(this);
+        plugin.getCommand("lserverinfo").setExecutor(this);
     }
 
-    @Command({"stopserver", "stopsrv", "lserver stop"})
-    @Description("Stops the server")
-    @Usage("/lserver start")
-    @CommandPermission("lightning.server")
-    public void stopServer(Player sender) {
-        if (!(Lightning.isOnline())) {
-            sender.sendMessage(ChatColor.RED + "Server is offline; cancelled.");
-            return;
-        }
-        Lightning.getHTTPServer().stop(0);
-    }
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		String path = cmd.getName() + (args.length < 1 ? "" : " " + args[0]);
+		
+		switch (path) {
+			case "lserver addlistener": {
+				if (!(sender.hasPermission("lightning.server.listener"))) {
+					sender.sendMessage(ChatColor.RED + "No Permission");
+					return false;
+				}
+				
+				if (args.length < 2) {
+		    		sender.sendMessage(ChatColor.RED + "Invalid URL Found");
+		    		return false;
+				}
+				
+		    	URI uri;
+		    	try {
+		    		uri = URI.create(args[1]);
+		    	} catch (Exception e) {
+		    		sender.sendMessage(ChatColor.RED + "Invalid URL Found");
+		    		return false;
+		    	}
+		    	
+		        if (Lightning.getEventListeners().contains(uri)) {
+		            sender.sendMessage(ChatColor.RED + "URL already exists");
+		            return false;
+		        }
+		        Lightning.addEventListener(uri);
+		        sender.sendMessage(ChatColor.GREEN + "Successfully added event listener to " + ChatColor.YELLOW + uri.toString());
+		   
+		        return true;
+			}
+			case "lserver":
+			case "lserverinfo":
+			case "lserver info": {
+				if (!(sender.hasPermission("lightning.server.info"))) {
+					sender.sendMessage(ChatColor.RED + "No Permission");
+					return false;
+				}
+				
+		        sender.sendMessage("Loading...");
 
-    @Command({"reloadserver", "reloadsrv", "lserver reload"})
-    @Description("Reloads the server, by stopping and starting")
-    @Usage("/lserver reload")
-    @CommandPermission("lightning.server")
-    public void reloadSever(Player sender) {
-        stopServer(sender);
-        startServer(sender);
-    }
+		        FileConfiguration config = plugin.getConfig();
+		        List<String> info = new ArrayList<>();
+		        
+		        info.add("=== HttpServer Information ===");
+		        info.add("Server Address: <ip>:" + config.getInt("port"));
+		        info.add(" ");
+		        info.add("=== HttpClient Information ===");
+		        info.add("Version: " + Lightning.client.version().name());
 
-    @Command({"addlistener", "lserver addlistener"})
-    @Description("Adds an event listener")
-    @Usage("/lserver addlistener <uri>")
-    @CommandPermission("lightning.server.listener")
-    public void addListener(Player sender, URI uri) {
-        if (Lightning.getEventListeners().contains(uri)) {
-            sender.sendMessage(ChatColor.RED + "URL already exists");
-            return;
-        }
-        Lightning.addEventListener(uri);
-        sender.sendMessage(ChatColor.GREEN + "Successfully added event listener to " + ChatColor.YELLOW + uri.toString());
-    }
-
-    @Command({"removelistener", "lserver removelistener"})
-    @Description("Adds an event listener")
-    @Usage("/lserver removelistener <uri>")
-    @CommandPermission("lightning.server.listener")
-    public void removeListener(Player sender, URI uri) {
-        if (!Lightning.getEventListeners().contains(uri)) {
-            sender.sendMessage(ChatColor.RED + "URL does not exist");
-            return;
-        }
-        Lightning.removeEventListener(uri);
-        sender.sendMessage(ChatColor.GREEN + "Successfully removed event listener from " + ChatColor.YELLOW + uri.toString());
-    }
-
-    @Command({"lserver", "lserver info", "lserverinfo"})
-    @Description("Query Lightning Server Information")
-    @Usage("/lserver info")
-    @CommandPermission("lightning.server.info")
-    public void getInformation(Player sender) {
-        sender.sendMessage("Loading...");
-
-        FileConfiguration config = plugin.getConfig();
-        List<String> info = new ArrayList<>();
-        
-        info.add("=== HttpServer Information ===");
-        info.add("Server Address: <ip>:" + config.getInt("port"));
-        info.add(" ");
-        info.add("=== HttpClient Information ===");
-        info.add("Version: " + Lightning.client.version().name());
-
-        sender.sendMessage(String.join("\n", info));
-    }
-
-    @Command({"startserver", "startsrv", "lserver start"})
-    @Description("Starts the server, if offline.")
-    @Usage("/lserver start")
-    @CommandPermission("lightning.server.start")
-    public void startServer(Player sender) {
-        if (Lightning.isOnline()) {
-            sender.sendMessage(ChatColor.RED + "Server is online; cancelled.");
-            return;
-        }
-        Lightning.getHTTPServer().start();
-    }
-
-    @Command({"setport", "setsrvport", "setserverport", "lserver port"})
-    @Description("Change the server port, with option to reload server.")
-    @Usage("/lserver port <port> [reload]")
-    @CommandPermission("lightning.server")
-    public void setPort(Player sender, @Range(min = 1, max = 65535) int port, @Switch boolean rl) {
-        FileConfiguration config = plugin.getConfig();
-
-        config.set("port", port);
-
-        if (rl) {
-            reloadSever(sender);
-            sender.sendMessage(ChatColor.GREEN + "Set port. You need to call /reloadserver for this to take effect.");
-        } else {
-            sender.sendMessage(ChatColor.GREEN + "Set port & Reloaded Server.");
-        }
-    }
-    
+		        sender.sendMessage(String.join("\n", info));
+				return true;
+			}
+			case "lserver removelistener": {
+				if (!(sender.hasPermission("lightning.server.listener"))) {
+					sender.sendMessage(ChatColor.RED + "No Permission");
+					return false;
+				}
+				
+				if (args.length < 2) {
+		    		sender.sendMessage(ChatColor.RED + "Invalid URL Found");
+		    		return false;
+				}
+				
+		    	URI uri;
+		    	try {
+		    		uri = URI.create(args[1]);
+		    	} catch (Exception e) {
+		    		sender.sendMessage(ChatColor.RED + "Invalid URL Found");
+		    		return false;
+		    	}
+		        if (!Lightning.getEventListeners().contains(uri)) {
+		            sender.sendMessage(ChatColor.RED + "URL does not exist");
+		            return false;
+		        }
+		    	Lightning.removeEventListener(uri);
+		    	sender.sendMessage(ChatColor.GREEN + "Successfully removed event listener from " + ChatColor.YELLOW + uri.toString());
+		    	return true;
+			}
+			default: {
+				sender.sendMessage(ChatColor.RED + "Invalid Arguments");
+				return false;
+			}
+		}
+	}
 }
